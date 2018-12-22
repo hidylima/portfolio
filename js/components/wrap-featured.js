dc93639236a88df8c559766aef089870a734f510
hidylima <hidy63@gmail.com>
2018-12-22 09:53:29 -0200

    upload de informção

--- js/components/wrap-featured.js
+++ js/components/wrap-featured.js
@@ -1,24 +1,14 @@
-function typeWriter(element){
-    var textArray = element.innerHTML.split('');
-    element.innerHTML = '';
-    textArray.forEach((letra, i) => {
-        
-        setTimeout(() => {
-            element.innerHTML += letra;
-        }, 100 * i);
-    });
-    
+function typeWriter(element) {
+  var textArray = element.innerHTML.split("");
+  element.innerHTML = "";
+  textArray.forEach((letra, i) => {
+    setTimeout(() => {
+      element.innerHTML += letra;
+    }, 100 * i);
+  });
+}
+function execute(element) {
+  return document.querySelector(element);
 }
 
-//  function typeWriter(elemento) {
-//     const textoArray = elemento.innerHTML.split('');
-//     elemento.innerHTML = '';
-//     for(let i = 0; i < textoArray.length; i++) {
-//       setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
-//     }
-//   }
-
-var title = document.querySelector('.wrap-featured .title');
-var subtitle = document.querySelector('.wrap-featured .subtitle');
-
-typeWriter(title);
+typeWriter(execute(".wrap-featured .subtitle"));
